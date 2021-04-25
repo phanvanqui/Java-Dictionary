85f0a9e75c2b349311861e60d3d1037e0f98f2ee
Qui Phan <pvqui0910@gmail.com>
2021-04-25 16:43:44 +0700

    commit 1

---
+++ TuDien.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.io.BufferedReader;
+
+
+
+public class TuDien {
+    final static String filePath = "slang.txt";
+
+    static Map<String,String> dictionary = new HashMap<String,String>();
+
+	static Scanner input = new Scanner(System.in);
+
+	public static void main(String args[]) throws IOException {
+
+        CreateDictionary();
+
+		var a = 0;
+		while (a != -1) {
+			System.out.println("----------------------------------");
+			System.out.println("Chon thao tac");
+			System.out.println("1: Search slang word | 2: Search definition | 3: Show history | 4: Add slangword | 5: Edit slang word");
+			System.out.println("6: Delete slang word | 7: Reset lang words | 8: Random slang word | 9: lang word quizz | 10: definition quizz");
+			System.out.print("selection input: ");
+			
+			a = input.nextInt();
+
+			if (a == 1) {
+				SearchSW();
+			}
+			// if (a == 2) {
+			// 	CapNhatHocSinh();
+			// }
+			// if (a == 3) {
+			// 	XoaHocSinh();
+			// }
+			// if (a == 4) {
+			// 	XemDanhSachHocSinh();
+			// }
+			// if (a == 5) {
+			// 	ExportHocSinh();
+			// }
+		}
+
+	}
+
+	public static void SearchSW(){
+		String value;
+
+		System.out.print("input slang word: ");
+		value = input.next();
+
+		var rs = dictionary.get(value);
+
+		if (rs == null || rs.isEmpty())
+			rs = dictionary.get(value.toUpperCase());
+
+
+		System.out.println("definition: "+ rs);
+
+	}
+
+
+    public static void CreateDictionary(){
+        String thisLine = null;
+      
+        try {
+           // open input stream test.txt for reading purpose.
+           BufferedReader br = new BufferedReader(new FileReader(filePath));
+           
+           while ((thisLine = br.readLine()) != null) {
+
+			  String[] str = thisLine.split("`");
+
+			  if (str.length < 2)
+			  	continue;
+
+			  dictionary.put(str[0], str[1]);
+
+           }       
+           br.close();
+		//    System.out.println(dictionary);
+        } catch(Exception e) {
+           e.printStackTrace();
+        }
+
+    }
+
+
+}
